da751cbcf99cd1440cd6cb9be324d92b4cb3cdf3
Ian Crawford <Ian@CrawfishIII>
2019-01-27 12:16:20 -0800

    Setup test database environment

---
+++ src/firebase/firebase.js
@@ -0,0 +1,120 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'Gum',
+//     note: '',
+//     amount: 195,
+//     createdAt: 0
+// });
+
+// database.ref('notes').push({
+//     title: 'course topics',
+//     body: 'React native, angular, python'
+// });
+
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((error) => {
+//         console.log('Error fetching data:', error);
+//     });
+
+// database.ref()
+//     .on('value', (snapshot) => {
+//         const { name, job } = snapshot.val();
+//         console.log(`${name} is a ${job.title} at ${job.company}.`);
+//     });
+
+// database.ref().set({
+//     name: 'Andrew Mead',
+//     age: 26,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Google'
+//     },
+//     stressLevel: 6,
+//     location: {
+//         city: 'Philadelphia',
+//         country: 'United State'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((error) => {
+//     console.log('This failed.', error);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
+
+// database.ref('isSingle')
+//     .remove()
+//     .then(() => {
+//         console.log('Removed isSingle from database');
+//     })
+//     .catch((error) => {
+//         console.log('Failed to remove isSingle:', error);
+//     });
